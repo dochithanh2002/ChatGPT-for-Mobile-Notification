9bd5829bf7f84b40722c2d65b92c93a2547a1e6c
Do Chi Thanh <chithanhdo2002@gmail.com>
2023-07-12 11:49:44 +0700

    Text refactor

--- pages/index.js
+++ pages/index.js
@@ -35,21 +35,21 @@ export default function Home() {
     <div>
       <Head>
         <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <link rel="icon" href="/logo.png" />
       </Head>
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Notification Generator</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="Enter an product"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate notification" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
